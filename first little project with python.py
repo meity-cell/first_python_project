d998146a528b85a189594e7725f7acbf6e45c5f9
meity-cell <mahdikheibari3@gmail.com>
2025-10-19 01:14:50 +0330

    Update first little project with python.py

--- first little project with python.py
+++ first little project with python.py
@@ -7,6 +7,6 @@ print("yourname.s lenght is :")
 print(len(name))
 print(" how old are you ? ")
 age= input(">") 
-
 print(" oh so cool you.ll be " + str(int(age)+1) + "in a year") 
 print(" it was such a pleasure for us to meet you hope we.ll meet again " + name ) 
+
