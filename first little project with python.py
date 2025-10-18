7eb6be1522ba493ad4527f2ffd155ab97bb7760f
meity-cell <mahdikheibari3@gmail.com>
2025-10-19 00:46:13 +0330

    Add files via upload

---
+++ first little project with python.py
@@ -0,0 +1,10 @@
+#this code make a machine that asks my name and age + it says the lenghth of my name and greets me 
+
+print(" whats your name? ") 
+name= input(">")
+print(" nice to meet you babe " + name) 
+print("yourname.s lenght is :")
+print(len(name))
+print(" how old are you ? ")
+age= input(">") 
+print(" oh so cool you.ll be " + str(int(age)+1) + "in a year") 
