2b1382ab19def45bb8a57fea33e4bfa59ac29d16
meity-cell <mahdikheibari3@gmail.com>
2025-10-24 00:58:19 +0330

    Update first little project with python.py
    more upgrades on my first machine

--- first little project with python.py
+++ first little project with python.py
@@ -1,12 +1,28 @@
-#this code make a machine that asks my name and age + it says the lenghth of my name and greets me 
-
-print(" whats your name? ") 
-name= input(">")
-print(" nice to meet you babe " + name) 
-print("yourname.s lenght is :")
-print(len(name))
-print(" how old are you ? ")
-age= input(">") 
-print(" oh so cool you.ll be " + str(int(age)+1) + "in a year") 
-print(" it was such a pleasure for us to meet you hope we.ll meet again " + name ) 
-
+print(" Hi how are you please provide your name so we can see your name ") 
+#Here asks for name and saves it 
+name=input(">") 
+print(" so cool " + name + " nice to meet you ")
+print (" did you know that your names length is ")
+print(len(name)) 
+#Here the machine mentions your names length
+print(" so " + name + "please tell me how old are you so i know that im talking to a young human or the death one LOL ") 
+age=int(input(">")) 
+print(" so you.re " +str(age) + "years old huh ?" )
+if age> 11:
+    print(" so your young enough to wtite some codes huh ? not bad young coder ") 
+else:
+    print(" ohhh its too soon for you to have this knowledge take some rest and then you can back back again its okay ") 
+print(" so i thing now i know your informations come back again young coder i.ll miss you " + name + " any words you wanna tell me?" + "[yes or no] ")
+word=input(">") 
+if word=="yes":
+    print(" so cool im open to any advice or words from you :D  " ) 
+    yourword=input(">")
+    print(" roger that :) ")
+elif word==no:
+    print(" Oh okay so i think its our end i think i.ll se you later " + name )
+print(" it was such a pleasure to meet you i.d be happy if you come back bye bye " + name + ("you can say goodbye to machine[goodbye] ") )
+word2=input(">")
+if word2=="goodbye":
+    print(" bye:) ")
+else: 
+    print(" :) ")
